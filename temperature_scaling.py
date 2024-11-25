2d60fdf448b12d49bca6b59aa8c0bc2ea39acec8
Ryan Pilgrim <rzpilgrim@gmail.com>
2024-11-25 17:15:12 -0500

    fix for one-hot labels and non-cuda devices

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -11,6 +11,7 @@ class ModelWithTemperature(nn.Module):
         NB: Output of the neural network should be the classification logits,
             NOT the softmax (or log softmax)!
     """
+
     def __init__(self, model):
         super(ModelWithTemperature, self).__init__()
         self.model = model
@@ -25,36 +26,41 @@ class ModelWithTemperature(nn.Module):
         Perform temperature scaling on logits
         """
         # Expand temperature to match the size of logits
-        temperature = self.temperature.unsqueeze(1).expand(logits.size(0), logits.size(1))
+        temperature = self.temperature.unsqueeze(1).expand(
+            logits.size(0), logits.size(1)
+        )
         return logits / temperature
 
     # This function probably should live outside of this class, but whatever
-    def set_temperature(self, valid_loader):
+    def set_temperature(self, valid_loader, device):
         """
         Tune the tempearature of the model (using the validation set).
         We're going to set it to optimize NLL.
         valid_loader (DataLoader): validation set loader
         """
-        self.cuda()
-        nll_criterion = nn.CrossEntropyLoss().cuda()
-        ece_criterion = _ECELoss().cuda()
+        self.to(device)
+        nll_criterion = nn.CrossEntropyLoss().to(device)
+        ece_criterion = _ECELoss().to(device)
 
         # First: collect all the logits and labels for the validation set
         logits_list = []
         labels_list = []
         with torch.no_grad():
-            for input, label in valid_loader:
-                input = input.cuda()
+            for input, label, _ in valid_loader:
+                input = input.to(device)
                 logits = self.model(input)
                 logits_list.append(logits)
                 labels_list.append(label)
-            logits = torch.cat(logits_list).cuda()
-            labels = torch.cat(labels_list).cuda()
+            logits = torch.cat(logits_list).to(device)
+            labels = torch.cat(labels_list).to(device)
 
         # Calculate NLL and ECE before temperature scaling
         before_temperature_nll = nll_criterion(logits, labels).item()
         before_temperature_ece = ece_criterion(logits, labels).item()
-        print('Before temperature - NLL: %.3f, ECE: %.3f' % (before_temperature_nll, before_temperature_ece))
+        print(
+            "Before temperature - NLL: %.3f, ECE: %.3f"
+            % (before_temperature_nll, before_temperature_ece)
+        )
 
         # Next: optimize the temperature w.r.t. NLL
         optimizer = optim.LBFGS([self.temperature], lr=0.01, max_iter=50)
@@ -64,13 +70,21 @@ class ModelWithTemperature(nn.Module):
             loss = nll_criterion(self.temperature_scale(logits), labels)
             loss.backward()
             return loss
+
         optimizer.step(eval)
 
         # Calculate NLL and ECE after temperature scaling
-        after_temperature_nll = nll_criterion(self.temperature_scale(logits), labels).item()
-        after_temperature_ece = ece_criterion(self.temperature_scale(logits), labels).item()
-        print('Optimal temperature: %.3f' % self.temperature.item())
-        print('After temperature - NLL: %.3f, ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
+        after_temperature_nll = nll_criterion(
+            self.temperature_scale(logits), labels
+        ).item()
+        after_temperature_ece = ece_criterion(
+            self.temperature_scale(logits), labels
+        ).item()
+        print("Optimal temperature: %.3f" % self.temperature.item())
+        print(
+            "After temperature - NLL: %.3f, ECE: %.3f"
+            % (after_temperature_nll, after_temperature_ece)
+        )
 
         return self
 
@@ -94,6 +108,7 @@ class _ECELoss(nn.Module):
     "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
     2015.
     """
+
     def __init__(self, n_bins=15):
         """
         n_bins (int): number of confidence interval bins
@@ -105,6 +120,7 @@ class _ECELoss(nn.Module):
 
     def forward(self, logits, labels):
         softmaxes = F.softmax(logits, dim=1)
+        _, labels = labels.max(dim=1)
         confidences, predictions = torch.max(softmaxes, 1)
         accuracies = predictions.eq(labels)
 
